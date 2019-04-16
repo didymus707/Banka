c1c4bbf1b9ef6e0cb1cd776e2cc2779d0f08bd6a
didymus707 <didymus7007@gmail.com>
2019-04-16 17:06:30 +0100

    ft(homepage):set homepage [Finishes #165378812]

--- js/usersignup.js
+++ js/usersignup.js
@@ -1,6 +1,16 @@
 (()=> {
     const form = document.querySelector('.form');
     const toggleLinks = document.getElementsByClassName("register");
+    const startBtn = document.querySelector('.main-story button');
+    const aside = document.querySelector('aside');
+    console.log(aside);
+
+    // get started with forms
+    startBtn.addEventListener('click', ()=> {
+        setTimeout(() => {
+            aside.style.display = 'block';  
+        }, 200);
+    })
 
     // toggle the forms
     toggleForms = ()=> {
